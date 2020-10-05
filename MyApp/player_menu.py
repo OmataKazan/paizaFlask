8911f06cd033102e1276a749b80bef25a07ace69
OmataKazan <42262680+OmataKazan@users.noreply.github.com>
2020-10-05 21:09:24 +0900

    行動メニュー2つの実装

--- MyApp/player_menu.py
+++ MyApp/player_menu.py
@@ -8,3 +8,13 @@ player = "勇者"
 @app.route("/")
 def menu():
     return render_template("menu.html",player=player)
+
+@app.route("/walk")
+def walk():
+    message = player + "は荒野を歩いていた"
+    return render_template("action.html",player=player,message=message)
+
+@app.route("/attack")
+def fight():
+    message = player + "は立派に戦い抜いた"
+    return render_template("action.html",player=player,message=message)
