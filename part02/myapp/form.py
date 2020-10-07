a3c3a247145e5a9891a60897cff970329afbbdfc
OmataKazan <42262680+OmataKazan@users.noreply.github.com>
2020-10-07 14:53:51 +0900

    GETメソッドでフォームを作った

--- part02/myapp/form.py
+++ part02/myapp/form.py
@@ -6,9 +6,13 @@ def show():
     message = "hello world"
     return render_template("form.html",message = message)
 
-@app.route("/result",methods=["POST"])
+@app.route("/result",methods=["GET","POST"])
 def result():
     message = "This is paiza"
-    airticle = request.form["article"] #formのテキストボックスのname属性に対応
-    name = request.form["name"]
+    if request.method == "POST":
+        airticle = request.form["article"] #formのテキストボックスのname属性に対応
+        name = request.form["name"]
+    else:
+        airticle = request.args.get("article")#データを取り出そう方法がPOSTと異なるので分離
+        name = request.args.get("name")
     return render_template("form.html",message=message,article=airticle,name=name)
