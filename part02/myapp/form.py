cfa1e5f34f6a17a079166aaf6a336d3e168eb97c
OmataKazan <42262680+OmataKazan@users.noreply.github.com>
2020-10-07 14:40:11 +0900

    POSTメソッドで値渡し

--- part02/myapp/form.py
+++ part02/myapp/form.py
@@ -1,7 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def show():
     message = "hello world"
     return render_template("form.html",message = message)
+
+@app.route("/result",methods=["POST"])
+def result():
+    message = "This is paiza"
+    airticle = request.form["article"] #formのテキストボックスのname属性に対応
+    name = request.form["name"]
+    return render_template("form.html",message=message,article=airticle,name=name)
